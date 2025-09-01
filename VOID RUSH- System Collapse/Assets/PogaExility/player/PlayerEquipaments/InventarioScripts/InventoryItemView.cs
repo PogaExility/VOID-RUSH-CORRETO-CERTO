219b1cd80d874b2bde274d34ed9ccbe9bb7a9df5
PogaExility <bernardoamorim19@gmail.com>
2025-08-31 23:37:29 -0300

    quase deu ruim

--- VOID RUSH- System Collapse/Assets/PogaExility/player/PlayerEquipaments/InventarioScripts/InventoryItemView.cs
+++ VOID RUSH- System Collapse/Assets/PogaExility/player/PlayerEquipaments/InventarioScripts/InventoryItemView.cs
@@ -30,7 +30,7 @@ public class InventoryItemView : MonoBehaviour,
         // âncora/pivô top-left para casar com o grid
         rectTransform.pivot = new Vector2(0f, 0f);
         rectTransform.anchorMin = new Vector2(0f, 0f);
-        rectTransform.anchorMax = new Vector2(1f, 1f);
+        rectTransform.anchorMax = new Vector2(0f, 1f);
 
         if (hoverOverlay != null) hoverOverlay.gameObject.SetActive(false);
 
