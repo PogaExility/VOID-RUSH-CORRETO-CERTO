3a5693e158815001ca16548998dbe953cccc282c
Vinícius <viniciusferreiradias123@gmail.com>
2025-11-19 11:43:15 -0300

    hgnfcbgvf

--- VOID RUSH- System Collapse/Assets/Vinishow/Scripts/ParallaxControler.cs
+++ VOID RUSH- System Collapse/Assets/Vinishow/Scripts/ParallaxControler.cs
@@ -1,26 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// É NECESSÁRIO ADICIONAR ESTA LINHA PARA O SCRIPT RECONHECER O CINEMACHINE
+using Unity.Cinemachine;
 
 public class ParallaxController : MonoBehaviour
 {
-    Transform cam; //Main Camera
-    Vector3 camStartPos;
-    float distance; //jarak antara start camera posisi dan current posisi
+    private Transform cam;
+    private Vector3 camStartPos;
+    private float distance;
 
-    GameObject[] backgrounds;
-    Material[] mat;
-    float[] backSpeed;
-
-    float farthestBack;
+    private GameObject[] backgrounds;
+    private Material[] mat;
+    private float[] backSpeed;
+    private float farthestBack;
 
     [Range(0.01f, 0.05f)]
     public float parallaxSpeed;
 
-    // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main.transform;
+        StartCoroutine(FindCameraAndInitialize());
+    }
+
+    private IEnumerator FindCameraAndInitialize()
+    {
+        // Variável temporária para encontrar o CinemachineBrain.
+        CinemachineBrain cinemachineBrain = null;
+
+        // Loop que roda até que o CinemachineBrain seja encontrado.
+        while (cinemachineBrain == null)
+        {
+            // MODIFICAÇÃO PRINCIPAL AQUI: Procuramos pelo componente, não pela tag.
+            cinemachineBrain = FindObjectOfType<CinemachineBrain>();
+
+            // Se não encontrou, espera até o próximo frame.
+            if (cinemachineBrain == null)
+            {
+                yield return null;
+            }
+        }
+
+        // --- Câmera encontrada! Agora pegamos o transform dela e continuamos ---
+        cam = cinemachineBrain.transform;
+
         camStartPos = cam.position;
 
         int backCount = transform.childCount;
@@ -39,7 +62,7 @@ public class ParallaxController : MonoBehaviour
 
     void BackSpeedCalculate(int backCount)
     {
-        for (int i = 0; i < backCount; i++) //find the farthest background
+        for (int i = 0; i < backCount; i++)
         {
             if ((backgrounds[i].transform.position.z - cam.position.z) > farthestBack)
             {
@@ -47,7 +70,7 @@ public class ParallaxController : MonoBehaviour
             }
         }
 
-        for (int i = 0; i < backCount; i++) //set the speed of bacground
+        for (int i = 0; i < backCount; i++)
         {
             backSpeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z) / farthestBack;
         }
@@ -55,6 +78,11 @@ public class ParallaxController : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (cam == null)
+        {
+            return;
+        }
+
         distance = cam.position.x - camStartPos.x;
         transform.position = new Vector3(cam.position.x, transform.position.y, 0);
 
@@ -64,6 +92,4 @@ public class ParallaxController : MonoBehaviour
             mat[i].SetTextureOffset("_MainTex", new Vector2(distance, 0) * speed);
         }
     }
-
-
 }
